5ea28c6acd046229ef87c12db76083d57476d704
Brandon Page <bpage@salesforce.com>
2017-11-10 10:34:51 -0800

    Test passing string from ruby to bash

--- .circleci/gitChangedLibs.rb
+++ .circleci/gitChangedLibs.rb
@@ -1,24 +1,21 @@
 require 'json'
+require 'set'
 
 $GITPRAPI = "https://api.github.com/repos/%s/SalesforceMobileSDK-android/pulls/%s/files"
-$LIBS = ["SalesforceAnalytics", "SalesforceHybridSDK", "SalesforceReact", "SalesforceSDKCore", "SmartStore", "SmartSync"]
+$libs = ["SalesforceAnalytics", "SalesforceHybridSDK", "SalesforceReact", "SalesforceSDKCore", "SmartStore", "SmartSync"]
 
 prFilesAPI = $GITPRAPI % [ENV["CIRCLE_PROJECT_USERNAME"], ENV["CIRCLE_PR_NUMBER"]]
-puts "prFIelsAPI: " + prFilesAPI
 curlCommand = "curl %s" % [prFilesAPI]
 pullfiles = `#{curlCommand}`
 prfiles = JSON.parse(pullfiles)
 
-# delete me
-puts "PR Files: " + prfiles.to_a().join(",")
-
 libs = Set.new
 for prfile in prfiles
   path = prfile["filename"]
-  for lib in $LIBS
+  for lib in $libs
     if path.include? lib
       libs = libs.add(lib)
     end
   end
 end
-puts "Libs:" + libs.to_a().join(",")
+puts libs.to_a().join(", ")
