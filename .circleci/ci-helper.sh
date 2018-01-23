a68b755d0b021fbe25d6aea94545a644cd027962
brandonpage <bpage1@salesforce.com>
2018-01-22 16:42:26 -0800

    Test nightly gradle memory issues fix.

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -77,9 +77,9 @@ function runTests {
             ./gradlew :libs:SalesforceReact:assemble
         else
             if [[ "${CURRENT_LIB}" == "RestExplorer" ]]; then
-                ./gradlew :native:NativeSampleApps:${CURRENT_LIB}:connectedAndroidTest -PdisablePreDex --continue --no-daemon --stacktrace
+                ./gradlew :native:NativeSampleApps:${CURRENT_LIB}:connectedAndroidTest -PdisablePreDex --continue --no-daemon --max-workers 2 --stacktrace
             else
-                ./gradlew :libs:${CURRENT_LIB}:connectedAndroidTest -PdisablePreDex --continue --no-daemon --stacktrace
+                ./gradlew :libs:${CURRENT_LIB}:connectedAndroidTest -PdisablePreDex --continue --no-daemon  --max-workers 2 --stacktrace
             fi
         fi
     else
