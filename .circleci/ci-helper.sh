eb97ce04c8b320b36d0c6edfd47ac77a7ad45c46
brandonpage <bpage1@salesforce.com>
2018-08-10 20:44:59 -0700

    fix pr reruns.  lots of cleanup

--- .circleci/ci-helper.sh
+++ .circleci/ci-helper.sh
@@ -21,11 +21,18 @@ function envSetup {
 function printTestsToRun {
     if [ -n "$NIGHTLY_TEST" ]; then
         echo -e "\n\nNightly -> Run everything."
-    elif [ -n "$CIRCLE_PULL_REQUEST" ]; then
+
+    # Check branch name since PR env vars are not present on manual re-runs.
+    elif [[ $CIRCLE_BRANCH == *"pull"* ]]; then
         LIBS_TO_TEST=$(ruby .circleci/gitChangedLibs.rb)
         echo -e "export LIBS_TO_TEST=${LIBS_TO_TEST}" >> "${BASH_ENV}"
         if [[ ! -z ${LIBS_TO_TEST} ]]; then
             echo -e "\n\nLibraries to Test-> ${LIBS_TO_TEST//","/", "}."
+
+            # Check if this is a test job that should continue
+            if [[ -z ${CURRENT_LIB} ]] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]]; then
+                circleci step halt
+            fi
         else
             echo -e "\n\nNothing to Test."
             circleci step halt
@@ -37,47 +44,38 @@ function printTestsToRun {
 }
 
 function runTests {
-    if ([ -n "$NIGHTLY_TEST" ] || ([ -n "$CIRCLE_PULL_REQUEST" ] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]])); then
-        
-        if ([ -n "$CIRCLE_PULL_REQUEST" ]); then
-            android_api=27
-        else
-            # Run API 21 on Mon, 23 on Wed, 25 on Fri
-            android_api=(19 + $(date +"%u"))        
-        fi
-
-        [[ $android_api < 23 ]] && device="Nexus6" || device="NexusLowRes"
-        gcloud firebase test android run \
-            --project mobile-apps-firebase-test \
-            --type instrumentation \
-            --app "native/NativeSampleApps/RestExplorer/build/outputs/apk/debug/RestExplorer-debug.apk" \
-            --test ${TEST_APK}  \
-            --device model=$device,version=$android_api,locale=en,orientation=portrait  \
-            --environment-variables coverage=true,coverageFile=/sdcard/tmp/code-coverage/connected/coverage.ec  \
-            --directories-to-pull=/sdcard/tmp  \
-            --results-dir=${CURRENT_LIB}-${CIRCLE_BUILD_NUM}  \
-            --results-history-name=${CURRENT_LIB}  \
-            --timeout 15m
-
-        mkdir -p firebase/results
-        gsutil -m cp -r -U "`gsutil ls gs://test-lab-w87i9sz6q175u-kwp8ium6js0zw/${CURRENT_LIB}-${CIRCLE_BUILD_NUM} | tail -1`*" ./firebase/
-        mv firebase/test_result_1.xml firebase/results
+    if ([ -n "$CIRCLE_PULL_REQUEST" ]); then
+        android_api=27
     else
-        echo "No need to run ${CURRENT_LIB} tests for this PR."
+        # Run API 21 on Mon, 23 on Wed, 25 on Fri
+        android_api=$((19 + $(date +"%u")))      
     fi
+
+    [[ $android_api < 23 ]] && device="Nexus6" || device="NexusLowRes"
+    gcloud firebase test android run \
+        --project mobile-apps-firebase-test \
+        --type instrumentation \
+        --app "native/NativeSampleApps/RestExplorer/build/outputs/apk/debug/RestExplorer-debug.apk" \
+        --test ${TEST_APK}  \
+        --device model=$device,version=$android_api,locale=en,orientation=portrait  \
+        --environment-variables coverage=true,coverageFile=/sdcard/tmp/code-coverage/connected/coverage.ec  \
+        --directories-to-pull=/sdcard/tmp  \
+        --results-dir=${CURRENT_LIB}-${CIRCLE_BUILD_NUM}  \
+        --results-history-name=${CURRENT_LIB}  \
+        --timeout 15m
+
+    mkdir -p firebase/results
+    gsutil -m cp -r -U "`gsutil ls gs://test-lab-w87i9sz6q175u-kwp8ium6js0zw/${CURRENT_LIB}-${CIRCLE_BUILD_NUM} | tail -1`*" ./firebase/
+    mv firebase/test_result_1.xml firebase/results
 }
 
 function runDanger {
-    if [ -n "$CIRCLE_PULL_REQUEST" ] && [[ ${LIBS_TO_TEST} == *"${CURRENT_LIB}"* ]]; then
-        if [ -z "${CURRENT_LIB}" ]; then
-            DANGER_GITHUB_API_TOKEN="5d42eadf98c58c9c4f60""7fcfc72cee4c7ef1486b" danger --dangerfile=.circleci/Dangerfile_PR.rb --danger_id=PR-Check --verbose
-        else
-            if ls libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/*.xml 1> /dev/null 2>&1; then
-                mv libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/*.xml libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/test-results.xml
-            fi
-            DANGER_GITHUB_API_TOKEN="5d42eadf98c58c9c4f60""7fcfc72cee4c7ef1486b" danger --dangerfile=.circleci/Dangerfile_Lib.rb --danger_id="${CURRENT_LIB}" --verbose
-        fi
+    if [ -z "${CURRENT_LIB}" ]; then
+        DANGER_GITHUB_API_TOKEN="5d42eadf98c58c9c4f60""7fcfc72cee4c7ef1486b" danger --dangerfile=.circleci/Dangerfile_PR.rb --danger_id=PR-Check --verbose
     else
-        echo "No need to run Danger."
+        if ls libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/*.xml 1> /dev/null 2>&1; then
+            mv libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/*.xml libs/"${CURRENT_LIB}"/build/outputs/androidTest-results/connected/test-results.xml
+        fi
+        DANGER_GITHUB_API_TOKEN="5d42eadf98c58c9c4f60""7fcfc72cee4c7ef1486b" danger --dangerfile=.circleci/Dangerfile_Lib.rb --danger_id="${CURRENT_LIB}" --verbose
     fi
 }
