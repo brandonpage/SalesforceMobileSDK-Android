1a7c823d6ffee6666aa0fcb41ab889b1f53398a7
Brandon Page <bpage@salesforce.com>
2025-01-28 18:43:34 -0800

    no message

--- .github/DangerFiles/StaticAnalysis.rb
+++ .github/DangerFiles/StaticAnalysis.rb
@@ -5,7 +5,7 @@
 warn("Big PR, try to keep changes smaller if you can.", sticky: true) if git.lines_of_code > 1000
 
 # Redirect contributors to PR to dev.
-fail("Please re-submit this PR to the dev branch, we may have already fixed your issue.", sticky: true) if github.branch_for_base != "dev"
+# fail("Please re-submit this PR to the dev branch, we may have already fixed your issue.", sticky: true) if github.branch_for_base != "dev"
 
 # Static Analysis
 android_lint.filtering = true
