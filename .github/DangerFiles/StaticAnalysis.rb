754dd62143fb25a8cc951dd2eca5d52a14af7eb5
Brandon Page <bpage@salesforce.com>
2025-01-28 20:47:18 -0800

    no message

--- .github/DangerFiles/StaticAnalysis.rb
+++ .github/DangerFiles/StaticAnalysis.rb
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env ruby
 
 # Warn when there is a big PR
@@ -8,8 +7,18 @@ warn("Big PR, try to keep changes smaller if you can.", sticky: true) if git.lin
 # fail("Please re-submit this PR to the dev branch, we may have already fixed your issue.", sticky: true) if github.branch_for_base != "dev"
 
 # Static Analysis
-print Dir.pwd
-Dir.chdir "../.."
-print Dir.pwd
-android_lint.filtering = true
-android_lint.lint(inline_mode: true)
+# List of Android libraries for testing
+LIBS = ['SalesforceAnalytics', 'SalesforceSDK', 'SmartStore', 'MobileSync', 'SalesforceHybrid', 'SalesforceReact']
+
+LIBS.each do |lib|
+    system("./gradlew libs:#{lib}:lint")
+
+    if File.file?("libs/#{lib}/build/reports/lint-results-debug.xml")
+        android_lint.skip_gradle_task = true
+        android_lint.report_file = "libs/#{lib}/build/reports/lint-results-debug.xml"
+        android_lint.filtering = true
+        android_lint.lint(inline_mode: true)
+    else
+        warn("No Lint Results for #{lib}.")
+    end
+end
