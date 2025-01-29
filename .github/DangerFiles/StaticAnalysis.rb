33c1849d65332ffe9be21fc1e229ffc1c3995313
Brandon Page <bpage@salesforce.com>
2025-01-28 21:41:40 -0800

    no message

--- .github/DangerFiles/StaticAnalysis.rb
+++ .github/DangerFiles/StaticAnalysis.rb
@@ -4,7 +4,7 @@
 warn("Big PR, try to keep changes smaller if you can.", sticky: true) if git.lines_of_code > 1000
 
 # Redirect contributors to PR to dev.
-# fail("Please re-submit this PR to the dev branch, we may have already fixed your issue.", sticky: true) if github.branch_for_base != "dev"
+
 
 # Static Analysis
 # List of Android libraries for testing
@@ -19,6 +19,6 @@ LIBS.each do |lib|
         android_lint.filtering = true
         android_lint.lint(inline_mode: true)
     else
-        warn("No Lint Results for #{lib}.")
+        fail("No Lint Results for #{lib}.")
     end
 end
