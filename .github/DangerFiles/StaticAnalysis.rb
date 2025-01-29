413687c17a720c6bbc12c371c24f10175ea0de34
Brandon Page <bpage@salesforce.com>
2025-01-29 11:30:47 -0800

    no message

--- .github/DangerFiles/StaticAnalysis.rb
+++ .github/DangerFiles/StaticAnalysis.rb
@@ -2,12 +2,13 @@
 
 warn("#{ENV['LIB']} Static Analysis Test")
 
-report_file_path = "../../libs/#{ENV['LIB']}/build/reports/lint-results-debug.xml"
+report_file_path = "libs/#{ENV['LIB']}/build/reports/lint-results-debug.xml"
 if File.file?(report_file_path)
     android_lint.skip_gradle_task = true
     android_lint.report_file = report_file_path
     android_lint.filtering = true
     android_lint.lint(inline_mode: true)
+    android_lint.lint()
 else
     fail("No Lint Results.")
 end
